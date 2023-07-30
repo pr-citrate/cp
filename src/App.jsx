098089b20b8ab67139ff015710f2f138d92e3dd6
pr-citrate <pr.citrate@gmail.com>
2023-07-31 03:06:11 +0900

    warn on refresh or back

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import { Route, Routes } from 'react-router-dom';
 import Loading from './pages/Loading';
 import { Suspense } from 'react';
 import React from 'react';
+import { useEffect } from 'react';
 
 const Landing = React.lazy(() => import('./pages/Landing'));
 const Test = React.lazy(() => import('./pages/Test'));
 const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function App() {
+  const preventClose = (e) => {
+    e.preventDefault();
+    e.returnValue = '';
+  };
+
+  useEffect(() => {
+    (() => {
+      window.addEventListener('beforeunload', preventClose);
+    })();
+
+    return () => {
+      window.removeEventListener('beforeunload', preventClose);
+    };
+  }, []);
+
   return (
     <Suspense fallback={<Loading />}>
       <Routes>
